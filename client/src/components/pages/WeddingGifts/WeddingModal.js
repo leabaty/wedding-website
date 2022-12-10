6e77114aee2a65a2a75d7da7726f5ef4ab03270b
Léa Baty <leabaty.dev@gmail.com>
2022-12-10 18:33:49 +0100

    Add form conditions

--- client/src/components/pages/WeddingGifts/WeddingModal.js
+++ client/src/components/pages/WeddingGifts/WeddingModal.js
@@ -8,14 +8,48 @@ import { IoCloseOutline } from 'react-icons/io5';
 export default function Modal(props) {
   const ref = useRef();
   const [isSubmit, setIsSubmit] = useState(false);
+  const [isSent, setIsSent] = useState(false);
   const [openedModal, setOpenedModal] = useState(false);
+  const [formErrors, setFormErrors] = useState({});
   const [formData, setFormData] = useState({
     firstname: '',
     lastname: '',
+    email: '',
     message: '',
     amount: '',
     activity: '',
   });
+
+  const validate = (value, maxAmount) => {
+    const errors = {};
+
+    const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/;
+    const regexAmount = /^\d+$/;
+
+    console.log(maxAmount);
+
+    if (!value.firstname) {
+      errors.firstname = "⚠ Allez, dis-moi comment tu t'appelles";
+    }
+    if (!value.lastname) {
+      errors.lastname = "⚠ Allez, dis-moi comment tu t'appelles";
+    }
+    if (!value.email) {
+      errors.email = "⚠ Promis je t'envoie pas de spams !";
+    } else if (!regexEmail.test(value.email)) {
+      errors.email = '⚠ Essaie encore, le format est bizarre non ?';
+    }
+    if (!value.amount) {
+      errors.amount = "⚠ Je crois qu'il manque un petit quelque chose...";
+    }
+    if (!regexAmount.test(value.amount)) {
+      errors.amount = '⚠ Tu sais ce qui est bon ? Les chiffres ronds !';
+    }
+    if (value.amount > maxAmount) {
+      errors.amount = `⚠ Pour atteindre notre but, il ne reste que ${maxAmount} € à compléter !`;
+    }
+    return errors;
+  };
 
   const updateParticipation = usePatchData('updateGift', props.props._id, {
     amount: +formData.amount,
@@ -32,7 +66,6 @@ export default function Modal(props) {
   //form filling
   const handleChange = (event) => {
     const value = event.target.value;
-
     setFormData({
       ...formData,
       [event.target.name]: value,
@@ -42,23 +75,32 @@ export default function Modal(props) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
     setIsSubmit(true);
+
+    const maxAmount = props.props.price - props.props.amount;
+    setFormErrors(validate(formData, maxAmount));
 
     setFormData((prevState) => {
       return {
         ...prevState,
       };
     });
-    sendData();
-    updateParticipation();
   };
 
   const sendData = () => {
+    setIsSent(true);
     console.log(formData);
   };
 
-  // modal
+  // if there are no errors and the form is Submit, send it
+  useEffect(() => {
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      updateParticipation();
+      sendData();
+    }
+  }, [formErrors]);
+
+  // modal behavior
   useEffect(() => {
     const closeModal = (e) => {
       if (openedModal && ref.current && !ref.current.contains(e.target)) {
@@ -86,7 +128,7 @@ export default function Modal(props) {
               <span className='close' onClick={close}>
                 <IoCloseOutline />
               </span>
-              {!isSubmit ? (
+              {!isSent ? (
                 <form className='form' onSubmit={handleSubmit}>
                   <div className='modal__content'>
                     <p className='title title--small'>{props.props.title}</p>
@@ -99,6 +141,7 @@ export default function Modal(props) {
                         onChange={handleChange}
                         value={formData.firstname}
                       />
+                      <p className='form__warning'>{formErrors.firstname}</p>
                     </div>
                     <div className='modal__element'>
                       {' '}
@@ -110,6 +153,20 @@ export default function Modal(props) {
                         onChange={handleChange}
                         value={formData.lastname}
                       />
+                      <p className='form__warning'>{formErrors.lastname}</p>
+                    </div>
+
+                    <div className='modal__element'>
+                      {' '}
+                      <input
+                        className='modal__input'
+                        placeholder='Email'
+                        type='text'
+                        name='email'
+                        onChange={handleChange}
+                        value={formData.email}
+                      />
+                      <p className='form__warning'>{formErrors.email}</p>
                     </div>
 
                     <div className='modal__element'>
@@ -133,6 +190,7 @@ export default function Modal(props) {
                         onChange={handleChange}
                         value={formData.amount}
                       />
+                      <p className='form__warning'>{formErrors.amount}</p>
                     </div>
 
                     <button
@@ -142,6 +200,12 @@ export default function Modal(props) {
                       Je participe !
                     </button>
                   </div>
+
+                  {Object.keys(formErrors).length !== 0 ? (
+                    <p>Essaie encore : Le formulaire contient des erreurs !</p>
+                  ) : (
+                    ''
+                  )}
                 </form>
               ) : (
                 <>
