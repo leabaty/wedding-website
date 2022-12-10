c4fbe27927c386adf7b499ec59920c15251579d7
Léa Baty <leabaty.dev@gmail.com>
2022-12-10 19:48:25 +0100

    Add fly.io config

--- server/index.js
+++ server/index.js
@@ -2,19 +2,24 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import * as dotenv from 'dotenv' 
+import * as dotenv from 'dotenv';
 
 import Router from './routes/router.js';
 
 const app = express();
 
-app.use(bodyParser.json({ limit : "30mb", extended: true}));
-app.use(bodyParser.urlencoded({ limit : "30mb", extended: true }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
-dotenv.config()
+dotenv.config();
 
 app.use('/', Router);
 
-mongoose.connect(process.env.CONNECTION_URL)
-  .then(() => app.listen(process.env.PORT, () => console.log(`Server Running on Port: http://localhost:${process.env.PORT}`)))
+mongoose
+  .connect(process.env.CONNECTION_URL)
+  .then(() =>
+    app.listen(process.env.PORT || 8080, () =>
+      console.log(`Server Running on Port: http://localhost:${process.env.PORT}`)
+    )
+  )
   .catch((error) => console.log(`${error} did not connect`));
