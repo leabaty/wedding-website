3fa0c07104340b4d0ec3c89748e07e3dbb8474b3
Léa Baty <lea@adalong.com>
2023-01-13 11:35:29 +0100

    Correct sorting order on WeddingGifts

--- client/src/components/pages/WeddingGifts/WeddingGift.js
+++ client/src/components/pages/WeddingGifts/WeddingGift.js
@@ -31,51 +31,53 @@ export const WeddingGift = () => {
       <div className='page gift__list'>
         {loading && <div>Chargement...</div>}
         {!loading &&
-          data.map((gift, index) => {
-            if (index < 20) {
-              return (
-                <div className='gift__card' key={gift._id}>
-                  <div className='gift__cover'>
-                    <img className='gift__img' src={gift.img_url} alt={gift.title} />
-                    <div className='gift__title'>
-                      <div className='gift__location-container'>
-                        <p className='gift__location'>{gift.location_id}</p>{' '}
+          data
+            .sort((a, b) => a.location_id - b.location_id)
+            .map((gift) => {
+              if (gift.location_id < 21) {
+                return (
+                  <div className='gift__card' key={gift._id}>
+                    <div className='gift__cover'>
+                      <img className='gift__img' src={gift.img_url} alt={gift.title} />
+                      <div className='gift__title'>
+                        <div className='gift__location-container'>
+                          <p className='gift__location'>{gift.location_id}</p>{' '}
+                        </div>
+                        <h1 className='gift__undertitle'>{gift.title}</h1>
                       </div>
-                      <h1 className='gift__undertitle'>{gift.title}</h1>
+                    </div>
+
+                    <div className='gift__infos'>
+                      <p className='gift__desc'>{gift.desc}</p>
+
+                      <div className='gift__details'>
+                        <div className='gift__status'>
+                          <p className='gift__price'>{gift.price} €</p>
+                          <p className='gift__amount'>{gift.amount || '-'} €</p>
+                          <p className='gift__participants'>{gift.participants || '-'} 👤</p>
+                        </div>
+
+                        <div className='gift__progress'>
+                          <div
+                            className='gift__progress-int'
+                            style={{
+                              width: calculateProgressBar(gift.amount, gift.price),
+                              backgroundColor: !gift.amount && 'var(--primary)',
+                            }}
+                          ></div>
+                        </div>
+
+                        {gift.amount >= gift.price ? (
+                          'Cadeau financé ! Merci pour votre participation !'
+                        ) : (
+                          <WeddingModal props={gift} />
+                        )}
+                      </div>
                     </div>
                   </div>
-
-                  <div className='gift__infos'>
-                    <p className='gift__desc'>{gift.desc}</p>
-
-                    <div className='gift__details'>
-                      <div className='gift__status'>
-                        <p className='gift__price'>{gift.price} €</p>
-                        <p className='gift__amount'>{gift.amount || '-'} €</p>
-                        <p className='gift__participants'>{gift.participants || '-'} 👤</p>
-                      </div>
-
-                      <div className='gift__progress'>
-                        <div
-                          className='gift__progress-int'
-                          style={{
-                            width: calculateProgressBar(gift.amount, gift.price),
-                            backgroundColor: !gift.amount && 'var(--primary)',
-                          }}
-                        ></div>
-                      </div>
-
-                      {gift.amount >= gift.price ? (
-                        'Cadeau financé ! Merci pour votre participation !'
-                      ) : (
-                        <WeddingModal props={gift} />
-                      )}
-                    </div>
-                  </div>
-                </div>
-              );
-            }
-          })}
+                );
+              }
+            })}
       </div>
 
       <h2 className='title'> Mexique </h2>
@@ -86,51 +88,53 @@ export const WeddingGift = () => {
       <div className='page gift__list'>
         {loading && <div>Chargement...</div>}
         {!loading &&
-          data.map((gift, index) => {
-            if (index > 19) {
-              return (
-                <div className='gift__card' key={gift._id}>
-                  <div className='gift__cover'>
-                    <img className='gift__img' src={gift.img_url} alt={gift.title} />
-                    <div className='gift__title'>
-                      <div className='gift__location-container'>
-                        <p className='gift__location'>{gift.location_id}</p>{' '}
+          data
+            .sort((a, b) => a.location_id - b.location_id)
+            .map((gift) => {
+              if (gift.location_id > 20) {
+                return (
+                  <div className='gift__card' key={gift._id}>
+                    <div className='gift__cover'>
+                      <img className='gift__img' src={gift.img_url} alt={gift.title} />
+                      <div className='gift__title'>
+                        <div className='gift__location-container'>
+                          <p className='gift__location'>{gift.location_id}</p>{' '}
+                        </div>
+                        <h1 className='gift__undertitle'>{gift.title}</h1>
                       </div>
-                      <h1 className='gift__undertitle'>{gift.title}</h1>
+                    </div>
+
+                    <div className='gift__infos'>
+                      <p className='gift__desc'>{gift.desc}</p>
+
+                      <div className='gift__details'>
+                        <div className='gift__status'>
+                          <p className='gift__price'>{gift.price} €</p>
+                          <p className='gift__amount'>{gift.amount || '-'} €</p>
+                          <p className='gift__participants'>{gift.participants || '-'} 👤</p>
+                        </div>
+
+                        <div className='gift__progress'>
+                          <div
+                            className='gift__progress-int'
+                            style={{
+                              width: calculateProgressBar(gift.amount, gift.price),
+                              backgroundColor: !gift.amount && 'var(--primary)',
+                            }}
+                          ></div>
+                        </div>
+
+                        {gift.amount >= gift.price ? (
+                          'Cadeau financé ! Merci pour votre participation !'
+                        ) : (
+                          <WeddingModal props={gift} />
+                        )}
+                      </div>
                     </div>
                   </div>
-
-                  <div className='gift__infos'>
-                    <p className='gift__desc'>{gift.desc}</p>
-
-                    <div className='gift__details'>
-                      <div className='gift__status'>
-                        <p className='gift__price'>{gift.price} €</p>
-                        <p className='gift__amount'>{gift.amount || '-'} €</p>
-                        <p className='gift__participants'>{gift.participants || '-'} 👤</p>
-                      </div>
-
-                      <div className='gift__progress'>
-                        <div
-                          className='gift__progress-int'
-                          style={{
-                            width: calculateProgressBar(gift.amount, gift.price),
-                            backgroundColor: !gift.amount && 'var(--primary)',
-                          }}
-                        ></div>
-                      </div>
-
-                      {gift.amount >= gift.price ? (
-                        'Cadeau financé ! Merci pour votre participation !'
-                      ) : (
-                        <WeddingModal props={gift} />
-                      )}
-                    </div>
-                  </div>
-                </div>
-              );
-            }
-          })}
+                );
+              }
+            })}
       </div>
     </>
   );
